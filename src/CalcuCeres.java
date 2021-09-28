04f3990dbb70355a67165441998c91642940c43a
marcianeri <marcia.neri@etec.sp.gov.br>
2021-09-27 22:49:03 -0300

    terceiro projeto inicial

---
+++ src/CalcuCeres.java
@@ -0,0 +1,12 @@
+public class CalcuCeres {
+
+    public static void main ( String[]args) {
+      int x= Integer.parseInt(args[1]);
+      int y= Integer.parseInt(args [2]);
+        }
+
+    static void sum (int x ,int y){
+        System.out.println(x+y);
+    }
+
+    }
